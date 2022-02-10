575d5a3942ecc481c19865d2098c3f9bd1ca4f34
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2022-02-10 15:29:29 +0700

    Product Discount Calculator

---
+++ src/main/java/DiscountServlet.java
@@ -0,0 +1,29 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "DiscountServlet", value = "/display-discount")
+public class DiscountServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        float price = Float.parseFloat(request.getParameter("price"));
+        int percent = Integer.parseInt(request.getParameter("percent"));
+
+        double discountAmount = price * percent*  0.01;
+        double discountPrice=price-discountAmount;
+
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<h1>Discount Amount: " + discountAmount + "</h1>");
+        writer.println("<h1>Discount Price: " + discountPrice + "</h1>");
+
+        writer.println("</html>");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -7,10 +7,21 @@
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
-  <head>
-    <title>$Title$</title>
-  </head>
-  <body>
-  $END$
-  </body>
+<head>
+  <title>Product Discount Calculator</title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+<h2>Product Discount Calculator</h2>
+<form method="post" action="/display-discount">
+  <label>Product Description: </label><br/>
+  <input type="text" name="description" /><br/>
+  <label>List Price: </label><br/>
+
+  <input type="text" name="price" /><br/>
+  <label>Discount Percent: </label><br/>
+  <input type="number" name="percent" placeholder="%" /><br/>
+  <input type = "submit" id = "submit" value = "Calculate Discount"/>
+</form>
+</body>
 </html>
