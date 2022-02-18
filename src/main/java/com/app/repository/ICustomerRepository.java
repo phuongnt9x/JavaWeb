b1983658303aef5477f4ae6404cd1a16087318f0
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2022-02-18 16:19:52 +0700

    6.customer jpa

---
+++ src/main/java/com/app/repository/ICustomerRepository.java
@@ -0,0 +1,6 @@
+package com.app.repository;
+
+import com.app.model.Customer;
+
+public interface ICustomerRepository extends IGeneralRepository<Customer> {
+}
---
+++ src/main/java/com/app/service/ICustomerService.java
@@ -0,0 +1,6 @@
+package com.app.service;
+
+import com.app.model.Customer;
+
+public interface ICustomerService extends IGeneralService<Customer> {
+}
