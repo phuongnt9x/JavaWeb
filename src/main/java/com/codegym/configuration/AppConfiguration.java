a26930c32a182bfe9bcbbc5f044cc42d4414e9e0
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2022-02-21 18:26:23 +0700

    custom phone validate

--- src/main/java/com/codegym/configuration/AppConfiguration.java
+++ src/main/java/com/codegym/configuration/AppConfiguration.java
@@ -3,9 +3,11 @@ package com.codegym.configuration;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -52,4 +54,10 @@ public class AppConfiguration implements WebMvcConfigurer,  ApplicationContextAw
         return viewResolver;
     }
 
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("validation-message");
+        return messageSource;
+    }
 }
