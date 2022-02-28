a5e2e8b65a3a08832c36363fe1f324227549d9f1
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2022-02-28 09:06:05 +0700

    x

--- src/main/java/com/app/controller/PostController.java
+++ src/main/java/com/app/controller/PostController.java
@@ -14,6 +14,7 @@ import org.springframework.web.servlet.ModelAndView;
 import java.util.Date;
 import java.util.Optional;
 
+@RequestMapping("/post")
 @Controller
 public class PostController {
 
---
+++ src/main/java/com/app/security/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.app.security;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/").permitAll()
+                .and()
+                .authorizeRequests().antMatchers("/post**").authenticated()
+                .and()
+                .authorizeRequests().antMatchers("/categories**").authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        super.configure(auth);
+    }
+}
