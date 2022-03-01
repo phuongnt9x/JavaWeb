8321e360ed0c345239a64fad34c8d5d445fe3943
phuongnt9x <95663567+phuongnt9x@users.noreply.github.com>
2022-03-01 11:46:21 +0700

    cm

--- src/main/java/com/app/security/SecurityConfig.java
+++ src/main/java/com/app/security/SecurityConfig.java
@@ -2,10 +2,18 @@ package com.app.security;
 
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("user").password("{noop}12345").roles("USER")
+                .and()
+                .withUser("admin").password("{noop}12345").roles("ADMIN");
+    }
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll()
@@ -17,10 +25,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .formLogin()
                 .and()
                 .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout"));
-    }
-
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
     }
 }
